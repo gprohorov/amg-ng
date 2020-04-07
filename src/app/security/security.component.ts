ee4168408842027addc07add3d918150fdd6f7f8
gprohorov <yuramail222@gmail.com>
2020-04-07 20:05:22 +0300

    Security component

---
+++ src/app/security/security.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {JwtClientService} from '../service/jwt-client.service';
+
+@Component({
+  selector: 'app-security',
+  templateUrl: './security.component.html',
+  styleUrls: ['./security.component.css']
+})
+export class SecurityComponent implements OnInit {
+
+  authRequest: any = {
+    username: 'admin',
+    password: 'admin'
+  };
+
+  accessToken: string;
+
+  constructor(private service: JwtClientService) { }
+
+  ngOnInit(): void {
+    this.getAccessToken(this.authRequest);
+  }
+
+
+  public getAccessToken(requestBody){
+    const response = this.service.generateToken(requestBody);
+    response.subscribe((data: HttpResponse<any>) => {
+      console.log(data.headers.get('authorization'));
+    });
+  }
+
+}
---
+++ src/app/service/jwt-client.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtClientService {
+
+  constructor(private http: HttpClient) { }
+
+  public generateToken(request){
+    return this.http.post('http://localhost:8080/login', request, {observe : 'response'});
+  }
+
+
+
+
+}
